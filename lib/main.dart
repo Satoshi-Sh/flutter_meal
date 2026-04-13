dfad126658ecee560c89ef8475c4ce0616ca75ee
satoshi-sh <satoss1108@gmail.com>
2026-04-12 19:42:25 -0500

    Finished 161 loading moeals data into a screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meal/data/dummy_data.dart';
 import 'package:flutter_meal/screens/categories.dart';
+import 'package:flutter_meal/screens/meals.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
@@ -23,7 +25,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: CategoriesScreen(),
+      home: MealsScreen(title: 'Some category...', meals: dummyMeals),
     );
   }
 }
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -10,7 +10,26 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Widget listContent = meals.isEmpty
-        ? const Text('No meal is available..')
+        ? Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Uh oh ... nothing here!',
+                  style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Try selecting a different category!',
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
+              ],
+            ),
+          )
         : ListView.builder(
             itemCount: meals.length,
             itemBuilder: (ctx, index) => Text(meals[index].title),
