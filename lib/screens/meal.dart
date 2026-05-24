ca927b90943a97d1974a06731f06c151ee176908
satoshi-sh <satoss1108@gmail.com>
2026-05-24 14:37:09 -0500

    Finished 205 configuring implicit animations

--- lib/screens/meal.dart
+++ lib/screens/meal.dart
@@ -29,11 +29,17 @@ class MealScreen extends ConsumerWidget {
               duration: const Duration(milliseconds: 300),
               transitionBuilder: (child, animation) {
                 return RotationTransition(
-                  turns: animation,
+                  turns: Tween<double>(
+                    begin: 0.8,
+                    end: 1.0,
+                  ).animate(animation),
                   child: child,
                 );
               },
-              child: Icon(isFavorite ? Icons.star : Icons.star_border),
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
             ),
             onPressed: () {
               final wasAdded = ref
